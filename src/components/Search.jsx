5e8fb704e96ae064f3d769a97b19019d28b19c53
Serg-Ale <sergioalexandre071@gmail.com>
2024-04-20 14:41:03 -0300

    add: initial implementation of api request

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+// Search.jsx
+import React, { useState } from "react";
+import axios from "axios";
 import SearchSelect from "./SearchSelect";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 const Search = () => {
-  const [searchType, setSearchType] = useState("");
+  const [searchType, setSearchType] = useState("character");
   const [searchTerm, setSearchTerm] = useState("");
+  const [responseItems, setResponseItems] = useState([]);
 
   const handleSelectChange = (selectedValue) => {
     setSearchType(selectedValue);
@@ -18,6 +21,18 @@ const Search = () => {
     console.log("Search term:", term);
   };
 
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://rickandmortyapi.com/api/${searchType}/?name=${searchTerm}`
+      );
+      console.log(response.data.results);
+      setResponseItems(response.data.results);
+    } catch (error) {
+      console.error("Error fetching characters:", error);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-3 m-auto bg-neutral-800 w-[90%] mt-8 p-4 rounded-xl">
       <SearchSelect onSelectChange={handleSelectChange} />
@@ -27,7 +42,17 @@ const Search = () => {
         value={searchTerm}
         onChange={handleInputChange}
       />
-      <Button variant="outline">Search</Button>
+      <Button variant="outline" onClick={handleSearch}>
+        Search
+      </Button>
+      <div>
+        <h2>Characters</h2>
+        <ul>
+          {responseItems.map((character) => (
+            <li key={character.id}>{character.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
