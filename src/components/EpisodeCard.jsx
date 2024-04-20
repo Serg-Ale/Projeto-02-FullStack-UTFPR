5bcc592332c390a801f4cdc9379b7509c767afa6
Serg-Ale <sergioalexandre071@gmail.com>
2024-04-20 16:02:30 -0300

    add: EpisodeCard structure

--- src/components/EpisodeCard.jsx
+++ src/components/EpisodeCard.jsx
@@ -1,5 +1,11 @@
 const EpisodeCard = ({ data }) => {
-  return <h1>Ep card</h1>;
+  return (
+    <div>
+      <h2>{data.name}</h2>
+      <p className="air-date">Air date - {data.air_date}</p>
+      <p className="episode">Episode - {data.episode}</p>
+    </div>
+  );
 };
 
 export default EpisodeCard;
