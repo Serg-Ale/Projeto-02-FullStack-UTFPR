a7bee50f7438b684ddb5d06eeed3c227003fc94a
Serg-Ale <sergioalexandre071@gmail.com>
2024-04-20 02:57:33 -0300

    add: initial Search internal components and handlesr

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,33 @@
-import { SearchSelect } from "./SearchSelect";
+import { useState } from "react";
+import SearchSelect from "./SearchSelect";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 const Search = () => {
+  const [searchType, setSearchType] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSelectChange = (selectedValue) => {
+    setSearchType(selectedValue);
+    console.log("Selected search type:", selectedValue);
+  };
+
+  const handleInputChange = (event) => {
+    const term = event.target.value;
+    setSearchTerm(term);
+    console.log("Search term:", term);
+  };
+
   return (
-    <div className="flex m-auto bg-neutral-800 w-[80%] mt-8 p-4 rounded-xl">
-      <SearchSelect />
+    <div className="flex flex-col gap-3 m-auto bg-neutral-800 w-[90%] mt-8 p-4 rounded-xl">
+      <SearchSelect onSelectChange={handleSelectChange} />
+      <Input
+        type="text"
+        placeholder="Enter a search term"
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <Button variant="outline">Search</Button>
     </div>
   );
 };
--- src/components/SearchSelect.jsx
+++ src/components/SearchSelect.jsx
@@ -1,27 +1,20 @@
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+const SearchSelect = ({ onSelectChange }) => {
+  const handleSelectChange = (event) => {
+    const selectedValue = event.target.value;
+    onSelectChange(selectedValue);
+  };
 
-export function SearchSelect() {
   return (
-    <Select>
-      <SelectTrigger>
-        <SelectValue placeholder="Select a search type" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Search Types</SelectLabel>
-          <SelectItem value="character">Character</SelectItem>
-          <SelectItem value="location">Location</SelectItem>
-          <SelectItem value="episode">Episode</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <select
+      id="searchType"
+      className="p-3 rounded-md text-sm"
+      onChange={handleSelectChange}
+    >
+      <option value="character">Character</option>
+      <option value="location">Location</option>
+      <option value="episode">Episode</option>
+    </select>
   );
-}
+};
+
+export default SearchSelect;
