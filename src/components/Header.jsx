6523983fea40f0e237a7d76aee5f3c8469a67ae6
Serg-Ale <sergioalexandre071@gmail.com>
2024-04-20 01:03:29 -0300

    add: Header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 const Header = () => {
-  return <h1>Rick and Morty API Search</h1>;
+  return (
+    <div className="flex p-8 bg-neutral-900 ">
+      <h1 className="text-4xl font-bold text-neutral-200 m-auto">
+        Rick and Morty API Search
+      </h1>
+    </div>
+  );
 };
 
 export default Header;
