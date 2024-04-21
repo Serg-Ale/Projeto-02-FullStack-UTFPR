97f2a8c7cf5329d4bd26ef24009c250e7c25786f
Serg-Ale <sergioalexandre071@gmail.com>
2024-04-21 15:41:30 -0300

    add: text-center for episode and location cards

--- src/components/EpisodeCard.jsx
+++ src/components/EpisodeCard.jsx
@@ -1,6 +1,6 @@
 const EpisodeCard = ({ data }) => {
   return (
-    <div className="p-3 text-lg ">
+    <div className="p-3 text-lg text-center">
       <h2 className="text-3xl font-bold group-hover:text-primary ">{data.name}</h2>
       <p className="air-date">Air date - {data.air_date}</p>
       <p className="episode">Episode - {data.episode}</p>
--- src/components/LocationCard.jsx
+++ src/components/LocationCard.jsx
@@ -1,6 +1,6 @@
 const LocationCard = ({ data }) => {
   return (
-    <div className="p-3 text-lg ">
+    <div className="p-3 text-lg text-center">
       <h2 className="text-3xl font-bold group-hover:text-primary">
         {data.name}
       </h2>
